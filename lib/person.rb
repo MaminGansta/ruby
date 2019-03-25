5410e532a6f64ce62b752ce8e24b558035940c59
Sanya <sanya@Sanyas-MacBook-Pro.local>
2019-03-25 20:33:03 +0300

    Fixed initialize

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
   attr_reader :name, :degree, :profession
 
-  def initialize(data)
-    @name = data['name']
-    @degree = data['degree']
-    @profession = data['profession']
+  def initialize(name, degree, profession)
+    @name = dname
+    @deg = degree
+    @prof = profession
   end
 
   def to_s
-     return "name is #{@name} deg #{@degree} prof #{@profession}"
+     "name is #{@name} deg #{@deg} prof #{@prof}"
   end
 end
--- lib/vacancy.rb
+++ lib/vacancy.rb
@@ -1,17 +1,17 @@
 class Vacancy
   attr_reader :name, :dep, :pay, :age, :deg, :prof
 
-  def initialize(data)
-    @name = data['name']
-    @dep  = data['dep']
-    @pay = data['pay']
-    @age = data['age']
-    @deg = data['deg']
-    @prof = data['prof']
+  def initialize(name, dep, pay, age, deg, prof)
+    @name = name
+    @dep  = dep
+    @pay = pay
+    @age = age
+    @deg = deg
+    @prof = prof
   end
 
   def to_s
-    return "#{name} #{dep} #{pay} #{age} #{deg} #{prof}"
+    "#{name} #{dep} #{pay} #{age} #{deg} #{prof}"
   end
 
 end
